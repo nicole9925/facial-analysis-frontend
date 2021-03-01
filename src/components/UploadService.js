fbe3884cb6e0fb2915e68eb192cc7ce941874070
Nicole <nlee@Nicoles-iMac.local>
2021-03-01 03:02:50 -0800

    change links

--- src/components/UploadService.js
+++ src/components/UploadService.js
@@ -8,7 +8,7 @@ class UploadService {
     data.append('file', parts[2].split(",")[1]);
     data.append('filename', name);
     
-    let response = await fetch('/api/upload/', {
+    let response = await fetch('https://facial-analysis-backend.herokuapp.com/api/upload/', {
       method: 'POST',
       body: data
     })
