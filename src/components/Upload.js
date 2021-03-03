c2e9576c687d38004ea5c26b89dd6d9621a5be3d
Nicole <nlee@Nicoles-iMac.local>
2021-03-03 00:13:18 -0800

    static frontend

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -30,7 +30,45 @@ const Upload = (props) => {
     const [raceIG, setRaceIG] = useState(null);
     const [genderIG, setGenderIG] = useState(null);
     const [ageIG, setAgeIG] = useState(null);
+    const [raceG, setRaceG] = useState(null);
+    const [genderG, setGenderG] = useState(null);
+    const [ageG, setAgeG] = useState(null);
+    const [raceBP, setRaceBP] = useState(null);
+    const [genderBP, setGenderBP] = useState(null);
+    const [ageBP, setAgeBP] = useState(null);
 
+    const [vizType, setVizType] = useState("grad");
+
+    const showViz = () => {
+        switch(vizType) {
+            case "grad":
+                return (<>
+                    <div className="ig-container">
+                        <img className = "image-container-ig" src={raceG} alt="Race Grad"></img>
+                        <img className = "image-container-ig" src={genderG} alt="Gender Grad"></img>
+                        <img className = "image-container-ig" src={ageG} alt="Age Grad"></img>
+                    </div>
+                </>)
+            case "ig":
+                return(<>
+                        <div className="ig-container">
+                            <img className = "image-container-ig" src={raceIG} alt="Race Integrated Grad"></img>
+                            <img className = "image-container-ig" src={genderIG} alt="Gender Integrated Grad"></img>
+                            <img className = "image-container-ig" src={ageIG} alt="Age Integrated Grad"></img>
+                        </div>
+                </>)
+            case "bp":
+                return(<>
+                        <div className="ig-container">
+                            <img className = "image-container-ig" src={raceBP} alt="Race Back Prop"></img>
+                            <img className = "image-container-ig" src={genderBP} alt="Gender Back Prop"></img>
+                            <img className = "image-container-ig" src={ageBP} alt="Age Back Prop"></img>
+                        </div>
+                </>)
+
+        }
+    }
+    
     const onImage = async (failedImages, successImages) => {
         try {
             setDisabled(false)
@@ -61,15 +99,25 @@ const Upload = (props) => {
 
     const submit = async () => {
         props.setProgress("Uploading");
-        const parts = img[0].split(';');
-        const name = parts[1].split('=')[1];
+        // const parts = img[0].split(';');
+        const name = '';
 
         const resp = await UploadService.UploadToServer(img, name)
         console.log(resp)
         setImageUrl('data:image/jpeg;base64,' + resp.pp_img)
+
         setRaceIG( 'data:image/jpeg;base64,' + resp.race_ig)
         setGenderIG( 'data:image/jpeg;base64,' + resp.gender_ig)
         setAgeIG( 'data:image/jpeg;base64,' + resp.age_ig)
+
+        setRaceG( 'data:image/jpeg;base64,' + resp.race_grad)
+        setGenderG( 'data:image/jpeg;base64,' + resp.gender_grad)
+        setAgeG( 'data:image/jpeg;base64,' + resp.age_grad)
+
+        setRaceBP( 'data:image/jpeg;base64,' + resp.race_back)
+        setGenderBP( 'data:image/jpeg;base64,' + resp.gender_back)
+        setAgeBP( 'data:image/jpeg;base64,' + resp.age_back)
+
         props.setProgress('Uploaded')
         props.setData({"race": resp.race, "gender": resp.gender, "age": resp.age})
         props.setPlotData1({"title": "Race Prediction", "data": resp.race_results, "colors": ["#9900ff", "#00ccff"]})
@@ -79,11 +127,10 @@ const Upload = (props) => {
         const progress = props.progress
         switch(progress) {
             case 'getUpload':
-                fetch('https://facial-analysis-backend.herokuapp.com/api/clear/')
                 return <>
                 <div className="upload-container">
                     <UploadComponent onImage={onImage} url={url} />
-                    <button className="submit" onClick={submit} disabled={disableButton}>Submit</button>
+                    <button className="submit" onClick={submit}>Submit</button>
                 </div>
                 </>
                 
@@ -103,18 +150,30 @@ const Upload = (props) => {
                         <div className="error-message">{errorMessage}</div>
                         <div className="upload-container">
                             <UploadComponent onImage={onImage} url={url} />
-                            <button className="submit" onClick={submit} disabled={disableButton}>Submit</button>
+                            <button className="submit" onClick={submit}>Submit</button>
                         </div>
                     </>
                 )
             case 'Analysis':
                 return (
-                <>
-                    <div className="ig-container">
-                        <img className = "image-container-ig" src={raceIG} alt="Race Integrated Grad"></img>
-                        <img className = "image-container-ig" src={genderIG} alt="Gender Integrated Grad"></img>
-                        <img className = "image-container-ig" src={ageIG} alt="Age Integrated Grad"></img>
+                <>  
+                <div className="grid">
+                    <div className="viz-headers">
+                        <h2>Race</h2>
+                        <h2>Gender</h2>
+                        <h2>Age</h2>
                     </div>
+
+                    {showViz()}
+
+                    <div className="viz-button-container">
+                        <button className="viz-button" name="grad" onClick={() => setVizType('grad')}>Grad-CAM</button>
+                        <button className="viz-button" name="ig" onClick={() => setVizType('bp')}>Guided BP</button>
+                        <button className="viz-button" name="bp" onClick={() => setVizType('ig')}>Integrated Grad</button>
+                    </div>                    
+                </div>
+
+
                 </>
                 )
             default:
--- src/components/UploadService.js
+++ src/components/UploadService.js
@@ -1,19 +1,37 @@
+import resolve from 'resolve';
+
 class UploadService {
   async UploadToServer(img, name) {
 
-    const data = new FormData();
+    // const data = new FormData();
 
-    const parts = img[0].split(';')
+    // const parts = img[0].split(';')
 
-    data.append('file', parts[2].split(",")[1]);
-    data.append('filename', name);
+    // data.append('file', parts[2].split(",")[1]);
+    // data.append('filename', name);
     
-    let response = await fetch('https://facial-analysis-backend.herokuapp.com/api/upload/', {
-      method: 'POST',
-      body: data
-    })
-    let resp_data = await response.json()
-    return resp_data
+    // let response = await fetch('https://facial-analysis-backend.herokuapp.com/api/upload/', {
+    //   method: 'POST',
+    //   body: data
+    // })
+    // let resp_data = await response.json()
+    // const resp = null;
+
+    // fetch('./text_data/image.json',{
+    //   headers : { 
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json'
+    //     }
+    // }.then(function(response) {
+    //     resp = response.json()
+    // }))
+    const data = await require('./image.json');
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 3000);
+    }).then(()=>{return data});
+    
 
 
 
